f962626e34b3524baa23d215ffb9d6e314b3d649
Alvin Steven <alvin.pin108@gmail.com>
2022-12-19 14:45:38 +0700

    alldata

---
+++ resources/views/crudpengepul.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.main')
+
+@section('container')
+      
+                <div class="content-wrapper">
+                    <!-- crud pengepul -->
+                    <div class="row">
+                        <div class="col-12 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Pendaftaran Pengepul</h4>
+                                    <form class="forms-sample">
+                                        <div class="form-group">
+                                            <label for="exampleInputnama">Nama</label>
+                                            <input type="text" class="form-control" id="exampleInputnama"
+                                                placeholder="Masukan Nama">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputalamat">Alamat</label>
+                                            <input type="email" class="form-control" id="exampleInputalamat"
+                                                placeholder="Alamat">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputkabupaten">Kabupaten</label>
+                                            <input type="email" class="form-control" id="exampleInputkabupaten"
+                                                placeholder="Kabupaten">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputtelepon">Telepon</label>
+                                            <input type="email" class="form-control" id="exampleInputtelepon"
+                                                placeholder="Masukan No Telepon">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputnoktp">No KTP</label>
+                                            <input type="email" class="form-control" id="exampleInputnoktp"
+                                                placeholder="Masukan No KTP">
+                                        </div>
+                                        <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                        <button class="btn btn-light">Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- crudpengepul  -->
+                </div>
+                <!-- modal tambah-->
+                <div class="modal fade" id="modaltambah" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="exampleModalLabel">Tambah Data Pemprosesan</h4>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form class="forms-sample" action="">
+                                    <div class="form-group">
+                                        <label for="vp_id">vp id</label>
+                                        <input type="text" class="form-control" id="vp_id" placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="gradea">Grade A</label>
+                                        <input type="text" class="form-control" id="gradea" placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="gradeb">Grade B</label>
+                                        <input type="text" class="form-control" id="gradeb" placeholder="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="gradec">Grade C</label>
+                                        <input type="text" class="form-control" id="gradec" placeholder="">
+                                    </div>
+                                    <div class="modal-footer" style="border:none;">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Batal</button>
+                                        <button type="button" class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,34 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('dashboard') }}">
+                <i class="mdi mdi-grid-large menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false"
+                aria-controls="form-elements">
+                <i class="menu-icon mdi mdi-cash-multiple"></i>
+                <span class="menu-title">Transaksi</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="form-elements">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="{{ url('menu/transaksibeli') }}">Transaksi Beli</a>
+                    </li>
+                </ul>
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="{{ url('menu/transaksijual') }}">Transaksi Jual</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ url('menu/proses') }}">
+                <i class="menu-icon mdi mdi-animation"></i>
+                <span class="menu-title">Pemprosesan</span>
+            </a>
+        </li>
+    </ul>
+</nav>
