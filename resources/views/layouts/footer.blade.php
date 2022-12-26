cc67a4dab4ed7b9bafd2f26fcb38794f9761000a
Alvin Steven <alvin.pin108@gmail.com>
2022-12-26 15:50:20 +0700

    deletes

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">cRM Pengepul</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Developer Team Website <i
-                class="ti-heart text-danger ml-1"></i></span>
-    </div>
-</footer>
